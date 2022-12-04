cffc6b8a6ab4090f5ed45d374ad1f3db1824c409
simonsisay <Simonsisay9@gmail.com>
2022-12-04 18:11:48 +0300

    feat/added blog meta tags

--- components/pages/blog/index.tsx
+++ components/pages/blog/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { RichText } from "prismic-reactjs";
 import { Query } from "../../../src/types/generated/graphql";
 import { Container } from "./styles";
@@ -7,8 +8,15 @@ type Props = {
 };
 
 const Blog = ({ blog }: Props) => {
+  const firstImage = blog?.content.find((item: any) => item.type === "image");
+
   return (
     <Container>
+      <Head>
+        <title>{blog?.title}</title>
+        <meta name="description" content={blog?.description} />
+        <meta property="og:image" content={firstImage?.url} />
+      </Head>
       <h1 className="main-header">{RichText.render(blog?.title)}</h1>
       <p className="excerpt">{RichText.render(blog?.description)}</p>
       <div className="content text">{RichText.render(blog?.content)}</div>
